3d4858a54a3f7175ff1d2e8db51a7fd659bea948
alihernandez <ali.hernandez9two@gmail.com>
2025-03-20 10:11:35 -0600

    trash

--- routes.js
+++ routes.js
@@ -3,49 +3,56 @@ const fs = require('fs');
 const requestHandler = (req, res) => {
   const url = req.url;
   const method = req.method;
-  if (url === '/') {
+
+  if (url === '/' && method === 'GET') {
     res.write('<html>');
     res.write('<head><title>Enter Message</title></head>');
     res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+      '<body><form action="/create-users" method="POST"><input type="text" name="username"><button type="submit">Submit</button></form></body>'
     );
     res.write('</html>');
     return res.end();
   }
-  
-  if (url === '/message' && method === 'POST') {
+
+  if (url === '/users') {
+    res.write('<html>');
+    res.write('<head><title>Usernames</title></head>');
+    res.write('<body><ul>');
+
+    let usernames = ['User1', 'User2', 'User3']; // More appropriate placeholder names
+    for (let name of usernames) {
+      res.write(`<li>${name}</li>`);
+    }
+
+    res.write('</ul></body>');
+    res.write('</html>');
+    return res.end();
+  }
+
+  if (url === '/create-users' && method === 'POST') {
     const body = [];
-    req.on('data', chunk => {
-      console.log(chunk);
+    req.on('data', (chunk) => {
       body.push(chunk);
     });
-    return req.on('end', () => {
+
+    req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
-      fs.writeFile('message.txt', message, err => {
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-      });
+      console.log(parsedBody);
+      res.statusCode = 302;
+      res.setHeader('Location', '/');
+      return res.end();
     });
+
+    return;
   }
+
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
-  res.write('<head><title>My First Page</title><head>');
+  res.write('<head><title>My First Page</title></head>');
   res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
   res.write('</html>');
-  res.end();
+  return res.end();
 };
-
-// module.exports = requestHandler;
-
-// module.exports = {
-//     handler: requestHandler,
-//     someText: 'Some hard coded text'
-// };
-
-// module.exports.handler = requestHandler;
-// module.exports.someText = 'Some text';
 
 exports.handler = requestHandler;
 exports.someText = 'Some hard coded text';
