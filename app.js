00781187826a038b706264c4eec02c8a834cc20d
alihernandez <ali.hernandez9two@gmail.com>
2025-03-20 16:06:38 -0600

    done

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-	console.log("in the middleware");
-	next();
+app.use('/add-product', (req, res, next) => {
+	console.log("in the 2nd middleware");
+	res.send('<h1>Add Product Page</h1>')
 });
-app.use((req, res, next) => {
-	console.log('in the next middleware');
+
+app.use('/', (req, res, next) => {
+	console.log("in the middleware");
+	res.send('<h1>Hello from express</h1>')
 });
 
 app.listen(3000);
