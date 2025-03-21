ce7b450a4c01713a8d694992e625132c78e26705
alihernandez <ali.hernandez9two@gmail.com>
2025-03-20 20:15:32 -0600

    serving html

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 const adminRoutes = require("./routes/admin");
 
 const shopRoutes = require("./routes/shop");
+const path = require("path");
 
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -14,7 +15,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
     });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,11 @@
+const path = require("path");
+
 const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
     console.log('In the "/" middleware');
-    res.send("<h1>Hello from Express App</h1>");
+    res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
   });
 
   module.exports = router;
