86054717e104b2c4ffa1eb60c0928f0aec26c11d
alihernandez <ali.hernandez9two@gmail.com>
2025-03-23 20:40:45 -0600

    pug template engine

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-const adminRoutes = require("./routes/admin");
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require("./routes/admin");
 
 const shopRoutes = require("./routes/shop");
 const path = require("path");
@@ -11,12 +14,13 @@ const path = require("path");
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.render('404', {docTitle: "Not Found"})
     });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -6,14 +6,19 @@ const rootDir = require("../util/path");
 
 const router = express.Router();
 
+const products = [];
+
 router.get("/add-product", (req, res, next) => {
-  console.log("in the middleware");
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  // console.log("in the middleware");
+  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render("add-product", {docTitle: "Add Products", path: "/admin/add-product"});
 });
 
 router.post("/add-product", (req, res, next) => {
+  products.push({title: req.body.title});
   console.log(req.body);
   res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
