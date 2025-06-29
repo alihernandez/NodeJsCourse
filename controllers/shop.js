26e247a3b9b14283ed624c13a87bfcf989c2af75
alihernandez <ali.hernandez9two@gmail.com>
2025-06-28 18:52:07 -0600

    delete product from cart functionality

--- controllers/shop.js
+++ controllers/shop.js
@@ -33,6 +33,7 @@ exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findById(prodId, (product) => {
     Cart.addProduct(prodId, product.price);
+    res.redirect("/cart"); 
   });
 };
 
@@ -47,10 +48,30 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render("shop/cart", {
+  Cart.getCart(cart => {
+	Product.fetchAll(products => {
+	const cartProducts = [];
+	for (product of products) {
+	const cartProductData = cart.products.find(prod => prod.id === product.id);
+		if(cartProductData) {
+			cartProducts.push({productData: product, qty: cartProductData.qty});
+		}
+	}
+	res.render("shop/cart", {
     path: "/cart",
     docTitle: "Your Cart",
+    products: cartProducts
   });
+	})
+});
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+	 const prodId = req.body.productId;
+	 Product.findById(prodId, product => {
+	 	 Cart.deleteProduct(prodId, product.price);
+	 	 res.redirect("/cart");
+	 });
 };
 
 exports.getOrders = (req, res, next) => {
